068229a878280789093c2f5025cead33a56ecc2b
亾亼 <9338740+gugewhite@user.noreply.gitee.com>
2022-10-09 00:52:38 +0800

    1

--- sql/init.sql
+++ sql/init.sql
@@ -23,5 +23,8 @@
 insert into `core_user` (`user_id`, `user_name`, `login_name`, `password`, `mobile`, `email`, `city`, `join_time`, `last_login`, `user_status`, `ct`, `ut`, `is_del`, `version`, `domain_id`, `office_phone`) values('1','admin','admin','123456','13634322998','1542422412@qq.com','漳州1',NULL,'2022-03-24 09:44:32','N',NULL,'2022-03-24 09:44:32','0','1241','1234',NULL);
 insert into `core_user` (`user_id`, `user_name`, `login_name`, `password`, `mobile`, `email`, `city`, `join_time`, `last_login`, `user_status`, `ct`, `ut`, `is_del`, `version`, `domain_id`, `office_phone`) values('2','gaomeng1','gaomeng1','123456','111221e',NULL,NULL,'2020-04-01 15:33:33','2021-04-20 09:57:04','N','2020-04-01 15:33:33','2021-04-20 09:57:04','0','111','1234',NULL);
 */
-
+create table 'class'(
+    'class_id' int (11),
+    'class_name' char (100)
+);
 insert into 'class'('class_id','class_name') values ('3','三班');
